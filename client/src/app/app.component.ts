917536eceff692364e4fc9222271efd4d636e5f6
tapixhx <tapish818@gmail.com>
2020-12-20 12:17:09 +0530

    Send message and leave room not integrated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,11 +12,22 @@ export class AppComponent implements OnInit {
 
   user:String;
   room:String;
+  messageText:any;
   messageArray:Array<{user:String, message:String}> = [];
 
   constructor(private socketService: SocketioService) {
+
     this.socketService.newUserJoined()
       .subscribe(data => this.messageArray.push(data));
+
+    this.socketService.userLeftRoom()
+      .subscribe(data => this.messageArray.push(data));
+
+    this.socketService.newMessageRecieved()
+      .subscribe((data) => {
+        this.messageArray.push(data);
+        console.log(this.messageArray);
+      });
   }
 
   ngOnInit() {
@@ -25,6 +36,14 @@ export class AppComponent implements OnInit {
 
   join() {
     this.socketService.joinRoom({user:this.user, room:this.room});
+  }
+
+  leave() {
+    this.socketService.leaveRoom({user:this.user, room:this.room});
+  }
+
+  send() {
+    this.socketService.sendMessage({user:this.user, room:this.room, message:this.messageText});
   }
 
 }
--- client/src/app/services/socketio.service.ts
+++ client/src/app/services/socketio.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-// import * as io from 'socket.io-client';
+// import * as io from 'socket.io-client/build/index';
 import {io} from 'socket.io-client/build/index';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
@@ -14,7 +14,7 @@ export class SocketioService {
   constructor() { }
 
   setupSocketConnection() {
-    this.socket = io(environment.SOCKET_ENDPOINT);
+    // this.socket = io(environment.SOCKET_ENDPOINT);
     this.socket.emit('message', 'Hello there from Angular.');
     this.socket.on('my broadcast', (data: string) => {
       console.log(data);
@@ -30,6 +30,36 @@ export class SocketioService {
   newUserJoined() {
     let observable = new Observable<{user: String, message: String}>(observer => {
       this.socket.on('new user joined', (data) => {
+        observer.next(data);
+      });
+      return () => {this.socket.disconnect();}
+    });
+    return observable;
+  }
+
+  leaveRoom(data) {
+    console.log(data);
+    this.socket.emit('leave', data);
+  }
+
+  userLeftRoom() {
+    let observable = new Observable<{user: String, message: String}>(observer => {
+      this.socket.on('left room', (data) => {
+        observer.next(data);
+      });
+      return () => {this.socket.disconnect();}
+    });
+    return observable;
+  }
+
+  sendMessage(data) {
+    console.log(data);
+    this.socket.emit('message', data);
+  }
+
+  newMessageRecieved() {
+    let observable = new Observable<{user: String, message: String}>(observer => {
+      this.socket.on('new message', (data) => {
         observer.next(data);
       });
       return () => {this.socket.disconnect();}
